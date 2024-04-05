a279e31d52eb5511b2ce0819cda4f8b39ff5357b
Erphun <heydar858@gmail.com>
2024-04-05 13:41:52 +0330

    changed logo link address in sign\(in,up) pages

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -4,16 +4,17 @@ import sD_logo from "../assets/Pics/logo-small(Dark).png"
 import sL_logo from "../assets/Pics/logo-small.png"
 import { useSelector } from "react-redux"
 import { Get_Theme } from "../Apps/Slices/Theme"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Logo = ({ className, small }: { className?: string; small?: boolean }) => {
     const Theme = useSelector(Get_Theme)
+    const { pathname } = useLocation()
 
     const lightLogoAddress = small ? sL_logo : L_logo
     const darkLogoAddress = small ? sD_logo : D_logo
 
     return (
-        <Link to={"/"}>
+        <Link to={pathname.includes("/sign") ? pathname : "/"}>
             <img
                 src={Theme === "dark" ? darkLogoAddress : lightLogoAddress}
                 alt='logo'
