acececb058814c6c31592d08b447a2bec58b4f36
Erphun <heydar858@gmail.com>
2024-04-06 20:45:37 +0330

    just moved a tag to sideBar component

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -3,15 +3,17 @@ import { LuPackage } from "react-icons/lu"
 
 const SideBar = () => {
     return (
-        <div className='dir-rtl'>
-            <div className='flex flex-col pt-3 px-0 gap-1.5 text-added-text-secondary'>
-                <div className='p-2 pr-2 flex gap-2 items-center hover:bg-added-text-primary hover:text-added-bg-primary group cursor-pointer rounded-bl-3xl rounded-tl-3xl'>
-                    <RiDashboard2Line className='text-2xl text-inherit' />
-                    <span>صفحه اصلی</span>
-                </div>
-                <div className='p-2 pr-2 flex gap-2 items-center hover:bg-added-text-primary hover:text-added-bg-primary group cursor-pointer rounded-bl-3xl rounded-tl-3xl'>
-                    <LuPackage className='text-2xl text-inherit' />
-                    <span>محصولات</span>
+        <div className={`overflow-y-auto dir-ltr h-[calc(100vh-var(--topBarHeight))] sideBarContainer`}>
+            <div className='dir-rtl'>
+                <div className='flex flex-col pt-3 px-0 gap-1.5 text-added-text-secondary'>
+                    <div className='p-2 pr-2 flex gap-2 items-center hover:bg-added-text-primary hover:text-added-bg-primary group cursor-pointer rounded-bl-3xl rounded-tl-3xl'>
+                        <RiDashboard2Line className='text-2xl text-inherit' />
+                        <span>صفحه اصلی</span>
+                    </div>
+                    <div className='p-2 pr-2 flex gap-2 items-center hover:bg-added-text-primary hover:text-added-bg-primary group cursor-pointer rounded-bl-3xl rounded-tl-3xl'>
+                        <LuPackage className='text-2xl text-inherit' />
+                        <span>محصولات</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -19,30 +19,68 @@ const Dashboard = () => {
                         sideBar ? "right-0" : `-right-sideBarWidth`
                     }`}
                 >
-                    <div className={`overflow-y-auto dir-ltr h-[calc(100vh-var(--topBarHeight))]`}>
-                        <SideBar />
-                    </div>
+                    <SideBar />
                 </div>
                 <div
                     className={`dir-ltr overflow-auto h-[calc(100vh-var(--topBarHeight))] bg-added-bg-primary w-full md:w-[calc(100vw-var(--sideBarWidth))]
                     `}
                 >
                     <div className='dir-rtl text-right'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque, quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut omnis, fugiat necessitatibus numquam
+                        recusandae impedit ipsum sequi quo ab corporis? Eveniet rem incidunt laboriosam odit similique nulla
+                        accusantium optio, fuga deleniti. Voluptas ducimus quasi culpa quos dolores alias repellat quas? Incidunt
+                        quas magni tempore dolorem accusantium provident enim recusandae non consectetur ducimus explicabo sed
+                        saepe inventore impedit, nulla odio delectus. Fuga blanditiis vitae architecto maiores, ut quam aut eaque,
+                        quaerat laudantium iusto ratione illo. Quibusdam incidunt optio expedita unde reiciendis.
                         <br />
                     </div>
                 </div>
