9feafb068c1607857123303ba05304d1ec6bce4f
Erphun <heydar858@gmail.com>
2024-04-10 18:28:57 +0330

    added children route and component for dashboard Page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,12 @@ import Login from "./Pages/Login"
 import Dashboard from "./Pages/Dashboard"
 import PrivateRoute from "./Components/PrivateRoute"
 import Register from "./Pages/Register"
+import Overview from "./Components/Overview"
+import Products from "./Components/Products"
+import AddProducts from "./Components/AddProducts"
+import Users from "./Components/Users"
+import AddUser from "./Components/AddUser"
+import Profile from "./Components/Profile"
 
 const Routes: RouteObject[] = [
     { path: "/signin", element: <Login /> },
@@ -14,6 +20,14 @@ const Routes: RouteObject[] = [
                 <Dashboard />
             </PrivateRoute>
         ),
+        children: [
+            { path: "", element: <Overview /> },
+            { path: "products", element: <Products /> },
+            { path: "add-product", element: <AddProducts /> },
+            { path: "users-list", element: <Users /> },
+            { path: "add-user", element: <AddUser /> },
+            { path: "profile", element: <Profile /> },
+        ],
     },
 ]
 
