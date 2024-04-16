0d17c2030e34ca26aae799ef07ddeda19e6b5010
Erphun <heydar858@gmail.com>
2024-04-16 20:41:41 +0330

    create get all products by createAsyncThunk and get all products

--- src/Apps/Slices/Products.ts
+++ src/Apps/Slices/Products.ts
@@ -13,17 +13,32 @@ export const AddProduct = createAsyncThunk("Products/AddProduct", async (arg: Ad
         .then(res => res.json())
         .then(res => res)
 })
+export const Get_Products = createAsyncThunk("Products/GetProducts", async () => {
+    return await fetch(`https://dashboard-a5184-default-rtdb.firebaseio.com/Products.json`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(res => res.json())
+        .then(res => res)
+})
 
 const ProductsSlice = createSlice({
     name: "Products",
     initialState,
     reducers: {},
     extraReducers: builder => {
-        builder.addCase(AddProduct.fulfilled, (state, action) => {
-            const id = action.payload.name
+        builder
+            .addCase(AddProduct.fulfilled, (state, action) => {
+                const id = action.payload.name
 
-            action.meta.arg.func(id)
-        })
+                action.meta.arg.func(id)
+            })
+            .addCase(Get_Products.fulfilled, (state, action) => {
+                const Products = Object.entries(action.payload) as T_Products
+                return Products
+            })
     },
 })
 
--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Get_Products, GettAllProducts } from "../Apps/Slices/Products"
+import { UnknownAction } from "@reduxjs/toolkit"
+
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+    const Dispatch = useDispatch()
+    const Products = useSelector(GettAllProducts)
+    useEffect(() => {
+        Dispatch(Get_Products() as unknown as UnknownAction)
+    }, [])
+    console.log(Products)
+    return <div>Products</div>
 }
 export default Products
