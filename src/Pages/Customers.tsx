bae9e1794c98e802a6d447406f22baef87cd8ca0
Erphun <heydar858@gmail.com>
2024-04-27 19:21:49 +0330

    added table and its style

--- src/Pages/Customers.tsx
+++ src/Pages/Customers.tsx
@@ -13,7 +13,6 @@ const Customers = () => {
     const [isShowAlert, setIsShowAlert] = useState<{ status: boolean; job: "DELETE" | "EDIT" }>({ status: false, job: "DELETE" })
     const Page_Ref = useRef<HTMLDivElement>(null)
 
-
     const _GetProducts_Handler = () => {
         setIsShowLoading(true)
         const removeLoading = () => {
@@ -97,7 +96,145 @@ const Customers = () => {
                     ) : null}
 
                     {Customers.length !== 0 ? (
-                        "customers"
+                        <div className='relative overflow-x-auto sm:rounded-lg p-1'>
+                            <div className='flex flex-column sm:flex-row flex-wrap items-center justify-between gap-2.5 pb-2.5 p-1'>
+                                <div className=''>
+                                    <button
+                                        className='items-center text-added-text-secondary bg-transparent border border-added-border hover:bg-added-bg-secondary font-medium rounded-md text-sm flex h-7 px-1.5'
+                                        type='button'
+                                    >
+                                        <svg
+                                            className='w-3 h-3 text-added-text-secondary me-3'
+                                            aria-hidden='true'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='currentColor'
+                                            viewBox='0 0 20 20'
+                                        >
+                                            <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z' />
+                                        </svg>
+                                        Last 30 days
+                                        <svg
+                                            className='w-2.5 h-2.5 ms-2.5'
+                                            aria-hidden='true'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 10 6'
+                                        >
+                                            <path
+                                                stroke='currentColor'
+                                                stroke-linecap='round'
+                                                stroke-linejoin='round'
+                                                stroke-width='2'
+                                                d='m1 1 4 4 4-4'
+                                            />
+                                        </svg>
+                                    </button>
+
+                                    <div className='z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow'>
+                                        <ul className='p-3 space-y-1 text-sm text-gray-700'>
+                                            <li>
+                                                <div className='flex items-center p-2 rounded hover:bg-gray-100'>
+                                                    <input
+                                                        id='filter-radio-example-1'
+                                                        type='radio'
+                                                        value=''
+                                                        name='filter-radio'
+                                                        className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300'
+                                                    />
+                                                    <label
+                                                        htmlFor='filter-radio-example-1'
+                                                        className='w-full ms-2 text-sm font-medium text-gray-900 rounded'
+                                                    >
+                                                        Last day
+                                                    </label>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='relative w-48 border  rounded-md h-7 border-added-border hover:bg-added-bg-secondary hasINput'>
+                                    <div className='absolute left-0.5 flex items-center top-1/2 -translate-y-1/2 overflow-hidden'>
+                                        <svg
+                                            className='w-5 h-5 text-added-text-secondary cursor-pointer'
+                                            aria-hidden='true'
+                                            fill='currentColor'
+                                            viewBox='0 0 20 20'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                        >
+                                            <path
+                                                fill-rule='evenodd'
+                                                d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+                                                clip-rule='evenodd'
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <input
+                                        type='text'
+                                        id='table-search'
+                                        className='w-10/12 h-full outline-none p-1 bg-transparent text-added-text-secondary'
+                                        placeholder='جستجو'
+                                    />
+                                </div>
+                            </div>
+
+                            <table className='text-sm w-[95%] m-auto text-left text-added-text-secondary outline outline-added-border rounded-md overflow-hidden'>
+                                <thead className='text-xs text-added-text-primary uppercase bg-added-border'>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='px-6 py-3'
+                                        >
+                                            Product name
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-6 py-3'
+                                        >
+                                            Color
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-6 py-3'
+                                        >
+                                            Category
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-6 py-3'
+                                        >
+                                            Price
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-6 py-3'
+                                        >
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className='bg-added-bg-secondary hover:bg-added-bg-primary'>
+                                        <th
+                                            scope='row'
+                                            className='px-6 py-4 font-medium text-added-text-primary/75 whitespace-nowrap '
+                                        >
+                                            Apple MacBook Pro 17"
+                                        </th>
+                                        <td className='px-6 py-4'>Silver</td>
+                                        <td className='px-6 py-4'>Laptop</td>
+                                        <td className='px-6 py-4'>$2999</td>
+                                        <td className='px-6 py-4'>
+                                            <a
+                                                href='#'
+                                                className='font-medium text-blue-600 hover:underline'
+                                            >
+                                                Edit
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     ) : (
                         <div className='text-center pb-10'>
                             <span className='border-b border-added-main inline-block mb-2 pb-1.5 md:text-lg lg:text-xl'>
