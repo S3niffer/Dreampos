b186769d76433fd7c2f35830fe31e746bdeca920
Erphun <heydar858@gmail.com>
2024-05-04 18:48:28 +0330

    handled getting products and customers and show their length in the boxes

--- src/Pages/Overview.tsx
+++ src/Pages/Overview.tsx
@@ -1,14 +1,27 @@
 import { LuBoxes, LuUsers } from "react-icons/lu"
 import OutLetParent from "../Components/OutLetParent"
+import { useDispatch, useSelector } from "react-redux"
+import { Get_Customers, GettAllCustomers } from "../Apps/Slices/Customers"
+import { useEffect } from "react"
+import { Get_Products, GettAllProducts } from "../Apps/Slices/Products"
+import { UnknownAction } from "@reduxjs/toolkit"
 
 const Overview = () => {
+    const Dispatch = useDispatch()
+    const Customers = useSelector(GettAllCustomers)
+    const Products = useSelector(GettAllProducts)
+
+    useEffect(() => {
+        Dispatch(Get_Customers(() => {}) as unknown as UnknownAction)
+        Dispatch(Get_Products(() => {}) as unknown as UnknownAction)
+    }, [])
     return (
         <OutLetParent>
             <div className='flex items-center justify-between p-4 sm:p-6 md:p-8 gap-8 sm:gap-12 md:gap-16 lg:justify-center'>
                 <div className='w-[min(50%,200px)] bg-[#ff9f43] rounded-md p-4 px-2 sm:px-4 text-white flex items-center justify-between'>
                     <div className='flex flex-col'>
                         <span className='inline-block text-center font-bold text-lg leading-4'>
-                            {(1).toLocaleString("fa-IR")}
+                            {Customers.length.toLocaleString("fa-IR")}
                         </span>
                         کاربران
                     </div>
@@ -17,7 +30,7 @@ const Overview = () => {
                 <div className='w-[min(50%,200px)] bg-[#28c76f] rounded-md p-4 px-2 sm:px-4  text-white flex items-center justify-between'>
                     <div className='flex flex-col'>
                         <span className='inline-block text-center font-bold text-lg leading-4'>
-                            {(1).toLocaleString("fa-IR")}
+                            {Products.length.toLocaleString("fa-IR")}
                         </span>
                         محصولات
                     </div>
